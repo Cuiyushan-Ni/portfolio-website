4d588750fa57a047ecfe19bd4ed5a4ef3036e9be
Cuiyushan-Ni <nicuiyushan@gmail.com>
2025-08-08 18:03:12 -0400

    Hero Page

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,18 +1,42 @@
+"use client";
 import React from 'react'
 import Image from "next/image"
+import { TypeAnimation } from 'react-type-animation';
+
+
 
 const HeroSection = () => {
   return (
     <section>
       <div className="grid grid-cols-1 sm:grid-cols-12">
         <div className="col-span-7 place-self-center text-center sm:text-left">
-          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">Hello, I'm Cuiyushan</h1>
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+              Hello, I'm {""}
+            </span>
+            <br></br>
+            <TypeAnimation
+              sequence={[
+                'Cuiyushan',
+                1000,
+                'Full-Stack Engineer',
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
+          </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
             blahblahblah
           </p>
           <div>
-            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-white hover:bg-slate-200 text-black">Hire Me</button>
-            <button className="px-6 py-3 w-full sm:w-fit rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3">Download Resume</button>
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
+              Hire Me
+            </button>
+            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-transparent bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3">
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">Download Resume</span>
+            </button>
           </div>
         </div>
         <div className="col-span-5 place-self-center mt-4 lg:mt-0">
