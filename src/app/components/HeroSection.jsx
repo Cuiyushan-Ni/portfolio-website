84d9815855cb2056472cbd4fffd4f7a3f82d5ad1
Cuiyushan-Ni <nicuiyushan@gmail.com>
2025-08-22 03:56:23 -0400

    Styling

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -9,17 +9,17 @@ const HeroSection = () => {
   return (
     <section>
       <div className="grid grid-cols-1 sm:grid-cols-12">
-        <div className="col-span-7 place-self-center text-center sm:text-left">
-          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+        <div className="col-span-8 place-self-center text-center sm:text-left md:justify-self-start">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-7xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
-              Hello, I'm {""}
+              Hello, I&apos;m{" "}
             </span>
             <br></br>
             <TypeAnimation
               sequence={[
-                'Cuiyushan',
+                "Cuiyushan / Remy",
                 1000,
-                'Full-Stack Engineer',
+                "Software Engineer",
                 1000,
               ]}
               wrapper="span"
@@ -39,14 +39,14 @@ const HeroSection = () => {
             </button>
           </div>
         </div>
-        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+        <div className="col-span-4 place-self-center mt-4 lg:mt-0">
+          <div className="relative rounded-full w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] bg-[radial-gradient(circle_at_center,_rgba(236,72,200,0.4)_0%,_rgba(236,72,200,0.2)_45%,_rgba(236,72,200,0)_100%)]">
             <Image
-              src="/images/hero-image.png"
+              src="/images/hero-image.gif"
               alt="hero image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              width={700}
-              height={700}
+              width={300}
+              height={300}
             />
           </div>
         </div>
